c0f3cdff211f3a4b0d1944549b1e5ee9469ce4a8
Piotr Papala <piotr.papala@gmail.com>
2016-06-20 09:53:05 +0200

    Model and server classes + configuration adjustments

---
+++ src/main/pl/futurity/javarestapp/Server.java
@@ -0,0 +1,34 @@
+package pl.futurity.javarestapp;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/student/data")
+public class Server {
+
+    @GET
+    @Path("/get")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Student getStudentRecord(){
+        Student student = new Student();
+        student.setFirstName("Elizabeth");
+        student.setLastName("Hayden");
+        student.setSchool("Little Flower");
+        student.setStandard("One");
+        student.setRollNumber(1113);
+        return student;
+    }
+
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response postStudentRecord(Student student){
+        String result = "Record entered: "+ student;
+        return Response.status(201).entity(result).build();
+    }
+}
